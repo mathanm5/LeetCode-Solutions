7e2f10c1e802c7451db00ca9a1fe783894dd4866
Mathanprasath K <mathancitra7@gmail.com>
2023-06-26 08:25:24 +0530

    Add files via upload

---
+++ Move Zeros/MoveZeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 0) return;
+        int left = 0;
+        for(int right = 0; right < nums.length; right++){
+            if(nums[right] != 0){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        for(int i = left; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
+//TC:O(n), SC:O(1)
