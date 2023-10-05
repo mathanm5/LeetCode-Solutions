69da9bee20c3ca906fbf3a91679286a60cc40b0e
Mathanprasath K <mathancitra7@gmail.com>
2023-10-05 06:28:20 +0530

    Add files via upload

--- Remove Duplicates From Sorted List/RemoveDuplicates.java
+++ Remove Duplicates From Sorted List/RemoveDuplicates.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        ListNode curr = head;
+        while(curr != null){
+        if(curr.next != null && curr.val == curr.next.val){
+            while(curr.next != null && curr.val == curr.next.val){
+                curr = curr.next;
+            }
+            prev.next = curr;
+            prev = curr;
+        }
+        else{
+            prev = curr;
+        }
+        curr = curr.next;
+        }
+        return dummyHead.next;
+    }
+}
+//TC:O(n), SC:O(1)
